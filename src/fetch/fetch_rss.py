59db9fd41302f77b0343864395f28f9f050d904b
hugopuybareau <hugopuybareau@github.com>
2025-02-13 18:08:26 +0100

    RSS fetch also works for Libération & fixed the PubDate issue

--- src/fetch/fetch_rss.py
+++ src/fetch/fetch_rss.py
@@ -9,10 +9,10 @@ from datetime import datetime
 # Target RSS feeds
 FEEDS = {
     "le_monde_politique" : "https://www.lemonde.fr/politique/rss_full.xml",
-    # "libération_politique" : "https://www.liberation.fr/rss/politiques/"
+    "libération_politique" : "https://www.liberation.fr/arc/outboundfeeds/rss-all/category/politique/?outputType=xml"
 }
 
-def get_article_content(url):
+def get_article_content(feed_name, url):
 
     try:
         response = requests.get(url, timeout=10)
@@ -20,12 +20,13 @@ def get_article_content(url):
 
         soup = BeautifulSoup(response.text, "html.parser")
 
-        paragraphs = soup.find_all("p", class_="article__paragraph")
-        full_text = "\n".join(p.get_text(strip=True) for p in paragraphs)
+        if feed_name == "le_monde_politique":
+            paragraphs = soup.find_all("p", class_="article__paragraph")
+            full_text = "\n".join(p.get_text(strip=True) for p in paragraphs)
 
-
-        # items = soup.find_all("item")
-        # full_text = "\n".join(item.get_text(strip=True) for item in items)
+        elif feed_name == "libération_politique":
+            paragraphs = soup.find_all("p", class_="sc-cPiKLX AiZTS text-element")
+            full_text = "\n".join(p.get_text(strip=True) for p in paragraphs)
 
         return full_text
     except Exception as e:
@@ -44,10 +45,10 @@ def fetch_and_save_rss(feeds, output_dir="data/raw"):
         for entry in parsed_feed.entries:
             title = entry.title if "title" in entry else "No Title" 
             link = entry.link if "link" in entry else "No Link"
-            pubDate = entry.pubDate if "pubDate" in entry else "No Date"
+            pubDate = entry.published if "published" in entry else "No Date"
             description = entry.description if "description" in entry else "No Description"
     
-            full_text = get_article_content(link)
+            full_text = get_article_content(feed_name, link)
 
             article_data = {
                 "feed_name": feed_name,
@@ -61,12 +62,12 @@ def fetch_and_save_rss(feeds, output_dir="data/raw"):
 
             time.sleep(1)
 
-        # Saving everything in a timestamped json file. 
-        timestamp = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
-        output_file = os.path.join(output_dir, f"french_politics_{timestamp}.json")
+    # Saving everything in a timestamped json file. 
+    timestamp = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+    output_file = os.path.join(output_dir, f"french_politics_{timestamp}.json")
 
-        with open(output_file, "w", encoding="utf-8") as f:
-            json.dump(all_articles, f, ensure_ascii=False, indent=2)
+    with open(output_file, "w", encoding="utf-8") as f:
+        json.dump(all_articles, f, ensure_ascii=False, indent=2)
     
     print(f"Saved {len(all_articles)} articles to {output_file}")
 
