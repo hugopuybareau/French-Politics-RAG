a7c93a8bc2fb1587e8afc3803dc7cc37d6746c0c
hugopuybareau <hugopuybareau@github.com>
2025-03-01 13:54:50 +0100

    retrieving function for the q&a pipeline

--- src/retrieval/retriever.py
+++ src/retrieval/retriever.py
@@ -1,3 +1,34 @@
 # src/retrieval/retriever.py
 
-from preprocess.faiss_setup import FaissIndex
+import os
+import faiss
+import json
+import numpy as np
+
+from ..embeddings.embedding import *
+from ..preprocess.faiss_setup import FaissIndex
+
+INDEX_DIR = os.path.join("data/index")
+INDEX_PATH = os.path.join(INDEX_DIR, "faiss.index")
+META_PATH = os.path.join(INDEX_DIR, "faiss_meta.json")
+
+# Load the Faiss index
+try:
+    print("[INFO Loading the Faiss index & metadata ...")
+    faiss_index = FaissIndex.load(INDEX_PATH, META_PATH)
+    print(f"[INFO] Loaded the index with {faiss_index.size} vectors.")
+except FileNotFoundError:
+    faiss_index = None
+    print("[INFO] Index not found.")
+
+
+def retrieve(query: str, ranks: int):
+    if not faiss_index:
+        print("[INFO] No index loaded.")
+        return []
+
+    query_vector = get_embedding(query)
+    results = faiss_index.search(query_vector, ranks)
+
+    return results
+
